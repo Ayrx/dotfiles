ad43cbc0259195c8b3300f21d7de5b746a00404d
Terry Chia <terrycwk1994@gmail.com>
2021-07-12 20:49:57 +0800

    Add more dotfiles.

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,48 @@
+set -x EDITOR nvim
+
+# Nix setup
+#set PATH ~/.nix-profile/bin $PATH
+#set -x NIX_PATH ~/.nix-defexpr/channels $NIX_PATH
+
+# Rust setup
+set PATH ~/.cargo/bin $PATH
+set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
+
+# fzf
+source ~/.nix-profile/share/fzf/key-bindings.fish && fzf_key_bindings
+set -x FZF_DEFAULT_COMMAND fd .
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x FZF_ALT_C_COMMAND fd -t d . $HOME
+
+# Others
+set -x PYTHONDONTWRITEBYTECODE 1
+set PATH ~/tools/bin $PATH
+set PATH ~/tools/ansible_bin $PATH
+set PATH ~/.nix-profile/bin $PATH
+
+if test (uname) = "Darwin"
+    set -x NIX_SSL_CERT_FILE /etc/ssl/cert.pem
+
+    if test (arch) = "arm64"
+        eval (/opt/homebrew/bin/brew shellenv)
+    else
+        eval (/usr/local/bin/brew shellenv)
+    end
+end
+
+# Functions
+function ara
+    if count $argv > /dev/null
+        7z a -t7Z $argv[1].7z $argv[1]
+    else
+        echo "Error: Supply file to archive"
+    end
+end
+
+function fish_greeting
+    fortune -a
+end
+
+# Aliases
+abbr -a autochrome ~/.local/autochrome/chrome --remote-debugging-port=9222
+abbr -a cat bat --paging=never
